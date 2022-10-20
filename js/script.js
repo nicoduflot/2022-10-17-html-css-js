205c61d804d3cd57f08938d8b566930bae6ef1d2
unknown <nduflot@dawan.fr>
2022-10-20 10:09:40 +0200

    javascript intro pt 1

--- js/script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('h1').addEventListener('click', function(){
-        console.log('clique sur le h1');
-        document.querySelector('h1').classList.toggle('red-h1');
-    });
-});
